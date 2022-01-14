6a2e502a74df79866ca1a718f19240c52de5a82e
Silva-Filho <tarusilva@gmail.com>
2022-01-14 20:11:24 -0300

    Resolução do exercício classe-02 da aula 10 de Front-end.

---
+++ classe-02/script.js
@@ -0,0 +1,10 @@
+const modal = document.querySelector(".modal");
+const imagemInterrogacao = document.querySelector(".interrogacao");
+
+const escondido = () => {
+    modal.classList.toggle("escondido");
+};
+
+/* imagemInterrogacao.addEventListener("click", escondido); */
+imagemInterrogacao.addEventListener("mouseenter", escondido);
+imagemInterrogacao.addEventListener("mouseleave", escondido);
